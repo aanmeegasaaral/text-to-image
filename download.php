2644418e0997f4852189f4dec4263d3334df5be2
Varun <varunsridharan23@gmail.com>
2020-07-29 21:05:13 +0530

    Updated

--- download.php
+++ download.php
@@ -1,5 +1,27 @@
 <?php
-$content = array();
+$current = time();
+$folder  = 'img/' . $current;
+mkdir( __DIR__ . '/' . $folder, 777, true );
+error_reporting( 0 );
+date_default_timezone_set( 'Asia/Kolkata' );
+global $content;
+
+$final_content = array();
+$content       = explode( PHP_EOL, $content );
+$i             = 0;
+
+foreach ( $content as $c ) {
+	if ( ! empty( $c ) ) {
+		$final_content[ $i ] = ( ! isset( $final_content[ $i ] ) ) ? '' : $final_content[ $i ];
+		$final_content[ $i ] .= trim( $c ) . PHP_EOL;
+	} else {
+		if ( isset( $final_content[ $i ] ) ) {
+			$final_content[ $i ] = trim( $final_content[ $i ] );
+		}
+		$i++;
+	}
+}
+
 
 require_once __DIR__ . '/vendor/autoload.php';
 
@@ -14,13 +36,29 @@ $client->getEngine()->setPath( __DIR__ . '/vendor/bin/phantomjs.exe' );
 $request = $client->getMessageFactory()->createCaptureRequest( 'https://aanmeegasaaral.pc/html.php', 'GET' );
 
 $i = 1;
-foreach ( $content as $id => $con ) {
+foreach ( $final_content as $id => $con ) {
 	$request->setRequestData( array( 'text' => urlencode( $con ) ) ); // Set post data
-	$request->setOutputFile( './output/' . $i . '.png' );
+	$request->setOutputFile( './' . $folder . '/' . $i . '.png' );
 	$request->setViewportSize( $width, $height );
 	$request->setCaptureDimensions( $width, $height, $top, $left );
 	$response = $client->getMessageFactory()->createResponse();
 	$client->send( $request, $response );
 	$i++;
-};
+}
 
+$files  = glob( './' . $folder . '/*', GLOB_NOSORT );
+$output = array();
+foreach ( $files as $file ) {
+	$id                      = basename( $file );
+	$output[ intval( $id ) ] = $_SERVER['REQUEST_SCHEME'] . '://' . $_SERVER['SERVER_NAME'] . '/index.php?download=' . urlencode( $folder . '/' . $id );
+}
+ksort( $output );
+echo '<br/><br/><div class="container">';
+foreach ( $output as $i => $val ) {
+	echo <<<HTML
+#${i} : <a href="${val}">${val}</a> <br/> <br/>
+HTML;
+
+}
+echo '</div>';
+exit;
---
+++ index.php
@@ -0,0 +1,38 @@
+<?php
+
+if ( isset( $_REQUEST['download'] ) ) {
+	$file = __DIR__ . './' . urldecode( $_REQUEST['download'] );
+	header( 'Content-type: octet/stream' );
+	header( 'Content-disposition: attachment; filename=' . basename( $file ) . ';' );
+	header( 'Content-Length: ' . filesize( $file ) );
+	readfile( $file );
+	exit;
+}
+?>
+
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css"
+	  integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+
+<?php
+if ( isset( $_REQUEST['content'] ) && isset( $_REQUEST['generate'] ) ) {
+	global $content;
+	$content = $_REQUEST['content'];
+	require_once __DIR__ . '/download.php';
+}
+?>
+
+<div class="container mt-5">
+	<form method="post">
+		<table class="table table-hover table-bordered">
+			<tr>
+				<td><textarea name="content" class="form-control" style="width:100%; min-height: 300px;"></textarea>
+				</td>
+			</tr>
+			<tr>
+				<td>
+					<button type="submit" name="generate" class="btn btn-success">Generate</button>
+				</td>
+			</tr>
+		</table>
+	</form>
+</div>
